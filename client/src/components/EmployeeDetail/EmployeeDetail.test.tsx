0d164a3f810b24c67d0c4e45fbd8478d668bd48c
Govind Brahmanyapura <brahmany@grinnell.edu>
2021-12-24 13:03:45 -0600

    fix user type issue

--- client/src/components/EmployeeDetail/EmployeeDetail.test.tsx
+++ client/src/components/EmployeeDetail/EmployeeDetail.test.tsx
@@ -1,1 +1,54 @@
-export {}
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Router, { BrowserRouter } from 'react-router-dom';
+import { User } from '../../types/User';
+import EmployeeDetail from './EmployeeDetail';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: jest.fn(),
+}));
+let useParamsSpy = jest.spyOn(Router, 'useParams');
+
+// const fakeEmployeesData: Array<User> = [
+//   { id: 1, name: { first: 'jim', last: 'john' }, job },
+// ];
+
+test('renders message and shows no employee data when no id present in path', () => {
+  useParamsSpy.mockReturnValue({ id: undefined });
+  render(
+    <BrowserRouter>
+      <EmployeeDetail
+        employees={[]}
+        setSelected={() => console.log('fake function')}
+        deleteEmployee={() => console.log('fake function')}
+      />
+    </BrowserRouter>
+  );
+  const noEmployeeSelected = screen.getByText(
+    'Select an employee to view more information'
+  );
+  expect(noEmployeeSelected).toBeInTheDocument();
+  useParamsSpy.mockClear();
+});
+
+test('renders employee data when valid id present in path', () => {
+  useParamsSpy.mockReturnValue({ id: '1' });
+
+  render(
+    <BrowserRouter>
+      <EmployeeDetail
+        employees={[]}
+        setSelected={() => console.log('fake function')}
+        deleteEmployee={() => console.log('fake function')}
+      />
+    </BrowserRouter>
+  );
+  const noEmployeeSelected = screen.getByText(
+    'Select an employee to view more information'
+  );
+  expect(noEmployeeSelected).toBeInTheDocument();
+  useParamsSpy.mockClear();
+});
+
+export {};
--- client/src/types/User.d.ts
+++ client/src/types/User.d.ts
@@ -2,43 +2,18 @@ export interface User {
   name: Name;
   location: Location;
   email: string;
-  dob: DOB;
   phone: string;
-  cell: string;
   id: number;
   picture: Picture;
-  nat: string;
   job: Job;
 }
 export interface Name {
-  title: string;
   first: string;
   last: string;
 }
 export interface Location {
-  street: Street;
   city: string;
-  state: string;
   country: string;
-  postcode: number;
-  coordinates: Coordinates;
-  timezone: Timezone;
-}
-export interface Street {
-  number: number;
-  name: string;
-}
-export interface Coordinates {
-  latitude: string;
-  longitude: string;
-}
-export interface Timezone {
-  offset: string;
-  description: string;
-}
-export interface DOB {
-  date: string;
-  age: number;
 }
 export interface Picture {
   large: string;
